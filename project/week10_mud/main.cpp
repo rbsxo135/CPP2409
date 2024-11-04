866f39daea6f393038bd1dddcf8a7a8434ec6924
rbsxo135 <a01096396942@gmail.com>
2024-11-04 17:40:58 +0900

    Mud-game2 first version

---
+++ project/week10_mud/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include "user.cpp"
+using namespace std;
+
+// 메인  함수
+int main() {
+	// 0은 빈 공간, 1은 아이템, 2는 적, 3은 포션, 4는 목적지
+	int map[mapY][mapX] = { 
+		{0, 1, 2, 0, 4},
+		{1, 0, 0, 2, 0},
+		{0, 0, 0, 0, 0},
+		{0, 2, 3, 0, 0},
+		{3, 0, 0, 0, 2} 
+	};
+	
+	// 유저 객체 생성
+	User user;
+
+	// 게임 시작 
+	while (1) { // 사용자에게 계속 입력받기 위해 무한 루프
+
+		// 사용자의 입력을 저장할 변수
+		string user_input = "";
+
+		cout << "현재 HP: " << user.getHP() << " 명령어를 입력하세요 (상,하,좌,우,지도,종료): ";
+		cin >> user_input;
+		
+		if (user_input == "상" || user_input == "하" || user_input == "좌" || user_input == "우"){
+			// 입력이 유효해 유저가 이동했을 경우 displayMap, checkState 메서드 호출
+			if(user.move(mapX, mapY, user_input) == true){
+				displayMap(map, user);
+				checkState(map, user);
+			}
+		}
+		else if (user_input == "지도") {
+			// TODO: 지도 보여주기 함수 호출
+			displayMap(map, user);
+		}
+		else if (user_input == "종료") {
+			cout << "종료합니다.";
+			break;
+		}
+		else {
+			cout << "잘못된 입력입니다." << endl;
+			continue;
+		}
+
+		// 유저의 hp가 0 이하인지 체크
+		if(checkUser(user) == false){
+			cout << "hp가 0이하가 되었습니다. 실패했습니다." << endl;
+			cout << "게임을 종료합니다." << endl;
+			break;
+		}
+
+		// 목적지에 도달했는지 체크
+		bool finish = checkGoal(map, user);
+		if (finish == true) {
+			cout << "목적지에 도착했습니다! 축하합니다!" << endl;
+			cout << "게임을 종료합니다." << endl;
+			break;
+		}
+	}
+	return 0;
+}
---
+++ project/week10_mud/user.h
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+const int mapX = 5;
+const int mapY = 5;
+
+class User{
+private:
+    int x = 0; // 가로 번호
+	int y = 0; // 세로 번호
+	int hp = 20; // 유저 체력
+	bool weapon = false; // 무기 가지고 있는지 여부
+	int armor = 0; // 갑옷 가지고 있는지 여부
+
+	bool checkXY(int mapX, int mapY, string user_input);
+public:
+    // private 멤버 변수를 리턴하는 get 메서드
+	int getX() const;
+	int getY() const;
+	int getHP() const;
+	bool getWeapon() const;
+	int getArmor() const;
+
+    // private 멤버 변수를 조작하는 메서드
+    bool move(int mapX, int mapY, string user_input);
+    void DecreaseHP(int dec_hp);
+    void IncreaseHP(int inc_hp);
+    void equip_weapon();
+    void equip_armor();
+    void lose_armor();
+
+};
+
+// 사용자 정의 함수
+// checkXY 메서드는 User 클래스 내부로 이식
+void displayMap(int map[][mapX], User user);
+bool checkGoal(int map[][mapX], User user);
+void checkState(int map[][mapX], User& user);
+bool checkUser(User user);
